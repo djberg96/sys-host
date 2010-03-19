b17db83ce151a4d2993d85a46de5673f9410ee84
Daniel Berger <djberg96@gmail.com>
2010-03-18 21:37:13 -0600

    Distill extconf.rb for Solaris.

--- ext/sunos/extconf.rb
+++ ext/sunos/extconf.rb
@@ -6,18 +6,8 @@ dir_config('host')
 have_func('gethostid')
 have_func('inet_ntop')
 have_func('gethostent_r')
-
-case Config::CONFIG['host_os']
-   when /bsd|darwin|powerpc|mach/i
-      have_func('getipnodebyname')
-   when /sunos|solaris/i
-      have_library('nsl')
-      have_library('socket')
-      have_func('gethostbyname_r', 'netdb.h')
-   when /linux/i
-      have_func('gethostbyname_r', 'netdb.h')
-      have_func('gethostent_r', 'netdb.h')
-   else
-end
+have_library('nsl')
+have_library('socket')
+have_func('gethostbyname_r', 'netdb.h')
 
 create_makefile('sys/host', 'sys')
