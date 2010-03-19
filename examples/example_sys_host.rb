8e705cc13c38bbb6615afb27c73cdbb2a88fd76f
Daniel Berger <djberge@caesar.(none)>
2010-03-19 12:47:46 -0600

    Fixed host_id method call.

--- examples/example_sys_host.rb
+++ examples/example_sys_host.rb
@@ -12,7 +12,7 @@ include Sys
 puts "VERSION: " + Host::VERSION
 puts "Hostname: " + Host.hostname
 puts "IP Addresses : " + Host.ip_addr.join(',')
-puts "Host ID: " + Host.hostid.to_s
+puts "Host ID: " + Host.host_id.to_s
 
 puts "Info: "
 
